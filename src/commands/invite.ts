8a1baf19e9afc170d6df7dc9cd21328f29628767
vuoreolprot <96374115+vuoreolprot@users.noreply.github.com>
2022-02-21 14:54:53 +0200

    Add files via upload

--- src/commands/invite.ts
+++ src/commands/invite.ts
@@ -5,26 +5,10 @@ export async function runInviteCommand(roomId: string, event: MessageEvent<Messa
     // The first argument is always going to be us, so get the second argument instead.
     let invite = args[1];
     if (invite.startsWith("@")) {
-        // Awesome! The user supplied an ID so we can create a proper mention instead
         const mention = await MentionPill.forUser(invite, roomId, client);
-        text = `/invite ${mention.text}`;
-        html = `/invite ${mention.html}`;
-    }
-
-    // Now send that message as a notice
-    return client.sendMessage(roomId, {
-        body: text,
-        msgtype: "m.notice",
-        format: "org.matrix.custom.html",
-        formatted_body: html,
-    });
-    
-}
-    else {
         return client.sendMessage(roomId, {
-            body: "Invite command syntac: \n e.g !bot invite USERNAMEHERE"
-            msgtype_ "m.notice",
-            format: "org.matrix.custom.html",
-            formatted_body: "<p>Invite command syntax: <br> e.g !bot invite USERNAMEHERE</p>",
-        });
+        body: "`/invite ${mention.text}`",
+        msgtype: "m.text",
+    });   
     }
+}
