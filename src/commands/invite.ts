d2e3dce2fb9cf0224d83fbcd7af88b2dde906842
vuoreolprot <96374115+vuoreolprot@users.noreply.github.com>
2022-02-21 13:40:20 +0200

    Add files via upload

--- src/commands/invite.ts
+++ src/commands/invite.ts
@@ -4,7 +4,6 @@ import * as htmlEscape from "escape-html";
 export async function runInviteCommand(roomId: string, event: MessageEvent<MessageEventContent>, args: string[], client: MatrixClient) {
     // The first argument is always going to be us, so get the second argument instead.
     let invite = args[1];
-
     if (invite.startsWith("@")) {
         // Awesome! The user supplied an ID so we can create a proper mention instead
         const mention = await MentionPill.forUser(invite, roomId, client);
@@ -19,4 +18,13 @@ export async function runInviteCommand(roomId: string, event: MessageEvent<Messa
         format: "org.matrix.custom.html",
         formatted_body: html,
     });
+    
 }
+    else {
+        return client.sendMessage(roomId, {
+            body: "Invite command syntac: \n e.g !bot invite USERNAMEHERE"
+            msgtype_ "m.notice",
+            format: "org.matrix.custom.html",
+            formatted_body: "<p>Invite command syntax: <br> e.g !bot invite USERNAMEHERE</p>",
+        });
+    }
