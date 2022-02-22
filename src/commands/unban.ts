63ab094276f2bc10e51d30dc2a9a9c8ed723d650
vuoreolprot <96374115+vuoreolprot@users.noreply.github.com>
2022-02-22 14:17:44 +0200

    Deleded files.

--- src/commands/unban.ts
+++
@@ -1,6 +0,0 @@
-import { MatrixClient, MentionPill, MessageEvent, MessageEventContent } from "matrix-bot-sdk";
-import * as htmlEscape from "escape-html";
-export async function runUnbanCommand(roomId: string, event: MessageEvent<MessageEventContent>, args: string[], client: MatrixClient) {
-    let userId = args[1];
-    client.unbanUser(userId, roomId);
-}
